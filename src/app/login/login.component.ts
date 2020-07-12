2d7ed4beeb2c4a13d8d8dc32d789acb4cec4e919
TEJASWI <bynagaritejaswi@gmail.com>
2020-07-12 11:42:29 +0530

    formatting

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavbarService } from '../services/navbar.service';
 import { LoginService } from '../services/login.service';
-import { NgForm,NgModel } from '@angular/forms';
+import { NgForm, NgModel } from '@angular/forms';
 import { Users } from '../model/Users';
 import { Toast, ToastRef, ToastrService } from 'ngx-toastr';
 
@@ -12,39 +12,39 @@ import { Toast, ToastRef, ToastrService } from 'ngx-toastr';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-id:number;
-role:string;
-  constructor(private router:Router,private userLogin:NavbarService
-    ,private toaster:ToastrService,private loginService:LoginService ) { }
+  id: number;
+  role: string;
+  constructor(private router: Router, private userLogin: NavbarService
+    , private toaster: ToastrService, private loginService: LoginService) { }
 
   ngOnInit() {
-    
-  }
-      Login(email,password){
-               
-            this.loginService.getUser(email.value,password.value).subscribe(x=>{
-              this.userLogin.user=x;
-              this.userLogin.selectedcat="THE CABIN";
-              this.UpdateInfo();
-            }); 
-            
 
-           
+  }
+  Login(email, password) {
+
+    this.loginService.getUser(email.value, password.value).subscribe(x => {
+      this.userLogin.user = x;
+      this.userLogin.selectedcat = "THE CABIN";
+      this.UpdateInfo();
+    });
+
+
+
+  }
+  UpdateInfo() {
+    if (this.userLogin.user) {
+      if (this.userLogin.user.role == 'Team') {
+        this.router.navigateByUrl('team/Index/manage-precheck');
+        this.userLogin.update(0);
+      } else if (this.userLogin.user.role == 'Customer') {
+        this.router.navigateByUrl('/manage-packages');
+        this.userLogin.update(1);
+      } else if (this.userLogin.user.role == 'Admin') {
+        this.router.navigateByUrl('admin/Index/manage-pakcage');
+        this.userLogin.update(0);
+      } else {
+        this.userLogin.update(0);
       }
-      UpdateInfo(){
-        if(this.userLogin.user){
-          if(this.userLogin.user.role=='Team'){
-            this.router.navigateByUrl('team/Index/manage-precheck'); 
-            this.userLogin.update(0);
-          } else if(this.userLogin.user.role=='Customer'){
-            this.router.navigateByUrl('/manage-packages');    
-            this.userLogin.update(1);
-          }else if(this.userLogin.user.role=='Admin'){
-            this.router.navigateByUrl('admin/Index/manage-pakcage');    
-            this.userLogin.update(0);
-          }else{
-            this.userLogin.update(0);
-          }
-        }
-      }
+    }
+  }
 }
